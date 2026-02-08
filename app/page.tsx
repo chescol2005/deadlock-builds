822befecf775e553b68ee3cb4c3328b78d08c347
Francesco LoCoco <lococo.fran@gmail.com>
2026-02-08 16:49:24 -0500

    Initial Deadlock build site

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 32 }}>
+      <h1>Deadlock Build Creator</h1>
+      <p>Your builds. Your stats. Your rules.</p>
+    </main>
+  );
+}
